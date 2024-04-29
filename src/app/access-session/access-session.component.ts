202af8b2d985129803bb5d6eba3e27554c62b7d3
lalgn <100622768+lalgn@users.noreply.github.com>
2024-04-29 17:32:02 +0200

    beau bouton

--- src/app/access-session/access-session.component.ts
+++ src/app/access-session/access-session.component.ts
@@ -1,41 +1,46 @@
-import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 import { Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { merge } from 'rxjs';
 
 @Component({
   standalone: true,
   selector: 'access-session',
   templateUrl: './access-session.component.html',
   styleUrl: './access-session.component.scss',
-  imports: [FormsModule,ReactiveFormsModule,NgIf]
+  imports: [FormsModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule]
 })
 export class AccessSessionComponent {
-  pinForm: FormGroup;
   router: Router = new Router;
 
-  constructor(private formBuilder: FormBuilder) {
-    // Initialize the form with form controls and validators
-    this.pinForm = this.formBuilder.group({
-      pin: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(6)]]
-    });
+  pin = new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(6)]);
+  errorMessage = '';
+
+  constructor() {
+    merge(this.pin.statusChanges, this.pin.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
   }
 
-  // Function to get access to form controls for easier validation in HTML
-  get formControls() {
-    return this.pinForm.controls;
-  }
-
-  // Function to handle form submission when the button is clicked
   valider() {
-    if (this.pinForm.valid) {
-      // Form is valid, perform desired action here (e.g., API call, navigate to another page, etc.)
-      console.log('PIN is valid:', this.pinForm.value.pin);
-      this.router.navigate(['/join']); // Fix: Use navigateByUrl instead of navigate
+    if (this.pin.valid) {
+      console.log('PIN is valid:', this.pin.value);
+      this.router.navigate(['/join']);
     } else {
-      // Form is invalid, mark all fields as touched to display validation errors
-      this.pinForm.markAllAsTouched();
+      this.pin.markAllAsTouched();
+    }
+  }
+
+  updateErrorMessage() {
+    if (this.pin.hasError('required')) {
+      this.errorMessage = 'Le code PIN est nécessaire';
+    } else {
+      this.errorMessage = 'Le code PIN doit être de 6 caractères';
     }
   }
 }
