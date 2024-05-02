a0507df587995616ee8a9e2c1e29b1efb0b55970
Entwaan <cremonaantoine@gmail.com>
2024-05-02 12:08:28 +0200

    waiting page + page selection plateau

--- src/app/waiting-for-players/waiting-for-players.component.ts
+++ src/app/waiting-for-players/waiting-for-players.component.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { NgFor } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { Equipe } from '../modele/equipe.model';
+import { WaitingForPlayersService } from './waiting-for-players.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-waiting-for-players',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, NgFor, MatCardModule],
   templateUrl: './waiting-for-players.component.html',
   styleUrl: './waiting-for-players.component.scss'
 })
 export class WaitingForPlayersComponent {
-  demarrer(){
+  listeEquipes: Equipe[] = [
+    {
+      id: 1, nom: "equipe1", score: 0,
+      partie: undefined
+    },
+    {
+      id: 2, nom: "equipe2", score: 0,
+      partie: undefined
+    },
+    {
+      id: 3, nom: "equipe3", score: 0,
+      partie: undefined
+    }
     
+  ]
+  constructor(private service : WaitingForPlayersService, private router : Router) { }
+
+
+  ngOnInit(){
+    this.service.ajouterEquipe((message) => {
+       console.log("json reçu",message);
+       if(!message.succes){
+          console.log(message.messageErreur);
+          this.router.navigate(['/error', message.codeErreur, message.messageErreur]);
+       }else{
+          let equipe = message.data as Equipe;
+          this.listeEquipes.push(equipe);
+       }
+    });
+  }
+
+  demarrer(){
+    this.service.demarrerPartie();
+  }
+
+  playersConnected(): boolean {
+    return !(this.listeEquipes.length === 0);
   }
 }
