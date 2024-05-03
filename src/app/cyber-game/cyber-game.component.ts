7b0911697ebf08baa2d21923a96a7293930cb9c5
cass1sse <101089486+cass1sse@users.noreply.github.com>
2024-05-03 15:18:05 +0200

    game

---
+++ src/app/cyber-game/cyber-game.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { MatCard } from '@angular/material/card';
+import { MatLabel } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButton } from '@angular/material/button';
+import { MatInput } from '@angular/material/input';
+
+@Component({
+  selector: 'app-cyber-game',
+  standalone: true,
+  imports: [MatCard, MatLabel, FormsModule, ReactiveFormsModule, MatButton, MatInput, MatFormFieldModule],
+  templateUrl: './cyber-game.component.html',
+  styleUrl: './cyber-game.component.scss'
+})
+export class CyberGameComponent {
+  password = new FormControl('', [Validators.required]);
+  message='';
+  validerMiniJeu(){
+
+    if(this.password.value == "gribouille"){
+      this.message = 'Félicitations ! Vous avez trouvé le bon mot de passe.';
+    } else {
+      this.message = 'Mot de passe invalide. Veuillez réessayer.';
+    }
+  }
+  
+
+}
