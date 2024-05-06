9cf099665870cdf8e594f3364af07d6369002476
lalgn <100622768+lalgn@users.noreply.github.com>
2024-05-06 10:48:25 +0200

    déconnexion du MDJ + fix sur les sockets

--- src/app/access-session/access-session.component.ts
+++ src/app/access-session/access-session.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import {
-  AbstractControl,
   FormControl,
   FormsModule,
   ReactiveFormsModule,
-  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
